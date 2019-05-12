36a9568bac1f18f079c2bcabccfc0700ade9090e
amruthasingh <amruthasingh.b@gmail.com>
2019-05-12 15:50:55 -0700

    updated scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-aws s3 cp s3://doorwatch-bucket/index.html /var/www/html/index.html
+!/bin/sh
+aws s3 cp s3://doorwatch-bucket-smart/index.html /var/www/html/index.html
 service httpd restart
